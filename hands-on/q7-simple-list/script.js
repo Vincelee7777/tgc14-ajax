21f3fb391a70e23f5b21fc934423e4320c28fcc4
Vincelee7777 <leeweixg2001@yahoo.com>
2021-09-01 11:04:09 +0000

    changes

--- hands-on/q7-simple-list/script.js
+++ hands-on/q7-simple-list/script.js
@@ -2,15 +2,25 @@ window.addEventListener('DOMContentLoaded', async function(){
     let response=await axios.get('https://anapioficeandfire.com/api/books')
     // console.log(response.data)
     let allbooks=response.data
-    document.querySelector('#button').addEventListener('click',function(){
+    document.querySelector('#button').addEventListener('click', async function(){
 
         for (let eachbook of allbooks){
-            console.log(eachbook)
+            // console.log(eachbook)
             document.querySelector('#name').innerHTML+="<li> Book name : "+ eachbook.name + "</li>"
             document.querySelector('#name').innerHTML+= "&nbsp&nbsp&nbsp&nbsp Number of pages :"+ eachbook.numberOfPages 
+            
+            for (let within of eachbook.povCharacters){
+                // for(let pov of within){
+                //     let response2=await axios.get(pov);
+                //     console.log(response2);
+                // }
+                
+                let character = (await axios.get(within)).data;
+                console.log(character.name);           
+            }
+
+
         }
-
-
 
 
     })
