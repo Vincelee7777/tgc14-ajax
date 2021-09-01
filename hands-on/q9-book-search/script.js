b5be9d942bef358dfa492683992f7a06131a4d81
Vincelee7777 <leeweixg2001@yahoo.com>
2021-09-01 16:18:33 +0000

    changes

--- hands-on/q9-book-search/script.js
+++ hands-on/q9-book-search/script.js
@@ -1,6 +1,17 @@
+const API_BASE_URL="https://openlibrary.org/search.json?"
+
 window.addEventListener('DOMContentLoaded', async function(){
-    let response=await axios.get('')
+    document.querySelector('#search').addEventListener('click', async function(){
+        let searchTerms = document.querySelector('#text2').value
+    let response=await axios.get(API_BASE_URL + 'title=' +searchTerms)
+    console.log(response)
+    // alert('abc')
+        for(let s of response.data.docs){
+            console.log(s)
+            document.querySelector('#name').innerHTML=`<li>Book Title: ${s.title}</li><li> Edition Count: ${s.edition_count}</li><li> Author Name: ${s.author_name[0]}</li>`
 
+        
+        }
 
-
+    })
 })
