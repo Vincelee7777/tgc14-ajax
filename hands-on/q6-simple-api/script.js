c611a3d902f08186c24a29bb9533434f630fdb77
Vincelee7777 <leeweixg2001@yahoo.com>
2021-09-01 14:33:28 +0000

    changes

--- hands-on/q6-simple-api/script.js
+++ hands-on/q6-simple-api/script.js
@@ -1,3 +1,18 @@
 
-let url = "https://programming-quotes-api.herokuapp.com/quotes/random"
+let url = " https://inspiration.goprogram.ai/" 
 
+
+window.addEventListener("DOMContentLoaded", async function(){
+    let response=await axios.get("https://pokeapi.co/api/v2/pokemon")
+
+    console.log(response.data)
+    document.querySelector('#load').addEventListener('click', function(){
+        for(let name of response.data.results){
+            // console.log(name)
+    document.querySelector('#content').innerHTML+=`<li>${name.name}</li>`
+
+        }
+
+
+})
+})
---
+++ hands-on/q9-book-search/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', async function(){
+    let response=await axios.get('')
+
+
+
+})
