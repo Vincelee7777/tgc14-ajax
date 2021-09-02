0e0f0cc1fde79fc55cff3a1b868f0264952677a1
Vincelee7777 <leeweixg2001@yahoo.com>
2021-09-02 03:24:23 +0000

    changes

--- hands-on/q6-simple-api/script.js
+++ hands-on/q6-simple-api/script.js
@@ -1,22 +1,44 @@
 
-let url = " https://inspiration.goprogram.ai/" 
+// let url = " https://api.kanye.rest/" 
+
+
+// window.addEventListener("DOMContentLoaded", async function(){
+//     let response=await axios.get("https://pokeapi.co/api/v2/pokemon")
+
+//     // console.log(response.data)
+//     document.querySelector('#load').addEventListener('click', function(){
+        
+//         for(n=0;n<10;n++){
+//             // for(let name of response.data.results){
+//                 let name=response.data.results[n]
+            
+//             console.log(response.data.results
+//                 )
+                
+//          document.querySelector('#content').innerHTML+=`<li>${name.name}</li>`
+        
+//         }
+
+        
+
+
+// })
+// })
+
+
 
 
 window.addEventListener("DOMContentLoaded", async function(){
-    let response=await axios.get("https://pokeapi.co/api/v2/pokemon")
+    let response=await axios.get("https://api.kanye.rest/")
 
-    console.log(response.data)
+    // console.log(response.data)
     document.querySelector('#load').addEventListener('click', function(){
         
-        // for(n=0;n<10;n++){
-            for(let name of response.data.results){
-                // let name=response.data.results[n]
-            
-            console.log(typeof name)
+        console.log(response.data)
                 
-         document.querySelector('#content').innerHTML+=`<li>${name.name}</li>`
+         document.querySelector('#content').innerHTML+=`<li>${response.data.quote}</li>`
         
-        }
+        
 
         
 
